37ffc1002bfcde0a04088456cca5dd846d0b474a
Mike <mike@heliotrope.digital>
2023-03-10 11:50:09 +0200

    Added design

---
+++ src/components/coverter-box/coverter-box.tsx
@@ -0,0 +1,11 @@
+import "./coverter-box.styles.css";
+
+function ConverterBox() {
+  return (
+    <div className="converter-box">
+      <p>frfr</p>
+    </div>
+  );
+}
+
+export default ConverterBox;
--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,17 +1,37 @@
 import { useEffect } from "react";
+import HeroImage from "../../assets/images/hero-converter-illustration.png";
+import ConverterBox from "../../components/coverter-box/coverter-box";
 import { useCurrenciesContext } from "../../entities/currency/currency-provider";
+import "./home-page.styles.css";
 
 function HomePage() {
-  const { currencies, getCurrencies } = useCurrenciesContext();
+  const { getCurrencies } = useCurrenciesContext();
 
   useEffect(() => {
     getCurrencies();
   }, [getCurrencies]);
 
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <section className="hero">
+        <div className="hero-container">
+          <div className="text-container">
+            <h1 className="font-bold text-white text-4xl">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Explicabo commodi nulla, quo quas dolorem
+            </h1>
+          </div>
+          <div className="image-container">
+            <img
+              src={HeroImage}
+              className="hero-illustration"
+              alt="Currency Converter"
+            />
+          </div>
+          <ConverterBox />
+        </div>
+      </section>
+    </>
   );
 }
 
